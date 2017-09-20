6b166ed76b828bc55eecfdd3ed77c08d2a44acce
ssokol1978 <31482099+ssokol1978@users.noreply.github.com>
2017-09-20 16:39:33 +0300

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,46 @@
-function massive(a) {
 
-	console.log(a);
+// function massive(a) {
 
-	return a.length;
+// 	for (var i=0; i<a.length; i++) {
+// 		console.log(a[i]);
+// 	}
+// 	return a.length;
+
+// }
+// var massivelength=massive([1, 2, 3]);
+// 	console.log(massivelength);
+
+// show();
+// function show() {
+// 	console.log("show");
+// }
+// function plus() {
+// 	var count=1;
+// 	return function() {
+// 		return ++count;
+// 	}
+// }
+
+// var counter = plus();
+// var counter1 = plus();
+// console.log(counter());
+
+//три функции и каждая принемает результат предедущей, функция возращает значение черерез return, а принимаем мы ее через вар или 26 строка
+
+
+
+
+
+
+
+function first() {
+	return 1;
 }
-
-massive([1, 2, 3]);
-
+function second() {
+    return 1+first();
+}
+function third() {
+	return 1+second();
+}
+var a=third();
+console.log(a);
